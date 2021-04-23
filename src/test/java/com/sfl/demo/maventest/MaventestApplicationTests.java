7231457bc5b84c7b0d502f3cd89f5cbdda94a1b9
“TraceNl” <18862233399@139.com>
2021-04-23 14:01:37 +0800

    first commit

---
+++ src/test/java/com/sfl/demo/maventest/MaventestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfl.demo.maventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaventestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
